c7664e77f21cfe055f0f6f301d91b68c192a8b72
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-13 20:01:29 +0100

    Initial Commit

---
+++ Twitter-NC1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Twitter-NC1
+//
+//  Created by Edgar Ernesto Vergara Montiel on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Twitter-NC1/Twitter_NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_NC1App.swift
+//  Twitter-NC1
+//
+//  Created by Edgar Ernesto Vergara Montiel on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
