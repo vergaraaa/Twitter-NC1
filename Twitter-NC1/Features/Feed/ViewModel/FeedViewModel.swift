73017388bb2e39f7f138e22dcc2fded9c4ee2eba
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-20 15:10:52 +0100

    feat: fetch tweets and refreshable

--- Twitter-NC1/Features/Feed/ViewModel/FeedViewModel.swift
+++ Twitter-NC1/Features/Feed/ViewModel/FeedViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class FeedViewModel: ObservableObject {
+    @Published var tweets = [Tweet]()
+    @Published var loading = true
+    
+    @MainActor
+    init() {
+        Task {
+            try await fetchTweets()
+            loading = false
+        }
+    }
+    
+    @MainActor
+    func fetchTweets() async throws {
+        tweets = try await TweetsService.fetchTweets()
+    }
+}
